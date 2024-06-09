4993a09808898d385fc62ec6cd68580e1171b01c
Somia Farag <somiafarag5@gmail.com>
2024-06-09 07:42:38 +0000

    lab completed

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -62,10 +62,22 @@ public class SampleClass {
     public int a;
     public boolean b;
 
-    //    implement a custom .equals(SampleClass other){} method here.
+    // Implement a custom .equals(SampleClass other){} method here.
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        SampleClass other = (SampleClass) obj;
+        return a == other.a && b == other.b;
+    }
 
-
-    //    implement a custom .toString(){} method here.
-
-    
+    // Implement a custom .toString(){} method here.
+    @Override
+    public String toString() {
+        return "SampleClass { a=" + a + ", b=" + b + " }";
+    }
 }
